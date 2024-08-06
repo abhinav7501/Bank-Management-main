b4ca7b056b9e2ffe4dd6de807e8f7fc6dc1947c3
abhinav7501 <abhinav7501@gmail.com>
2024-08-06 22:29:47 +0530

    final commit

--- banking_system/__init__.py
+++ banking_system/__init__.py
@@ -4,7 +4,7 @@ from os import environ
 
 app = Flask(__name__)
 app.secret_key = environ.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root@localhost:3306/res_banking'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SALT'] = environ.get('SALT')
 db = SQLAlchemy(app)
